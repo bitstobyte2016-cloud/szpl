2d6bfa92916ff2b53e784e189f2e066e55b72673
aradh <aradh@Aana-X1>
2026-05-13 12:23:55 +0530

    changes to pages

--- app/Views/products.php
+++ app/Views/products.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products - Kpopmerch.in</title>
+    <title>Products - SZPL</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/theme.css">
 </head>
@@ -13,8 +13,7 @@
         <h1>Products</h1>
         <p>This is the Products page.</p>
     </main>
-    <footer>
-        <p>&copy; 2024 Kpopmerch.in. All rights reserved.</p>
-    </footer>
+    
+    <?= view('footer') ?>
 </body>
 </html>
